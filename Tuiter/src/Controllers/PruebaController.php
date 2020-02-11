e51c8e24fbe71d51246d3b1ea005ae847887e701
roberz <roberzaccarello@gmail.com>
2020-02-11 13:15:37 -0300

    agregando mi primer controller

--- Tuiter/src/Controllers/PruebaController.php
+++ Tuiter/src/Controllers/PruebaController.php
@@ -5,21 +5,18 @@ namespace Tuiter\Controllers;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-class IndexController implements \Tuiter\Interfaces\Controller {
+class PruebaController implements \Tuiter\Interfaces\Controller{
 
     public function config($app) {
-
-        $app->get('/', function (Request $request, Response $response, array $args) {
+        $app->get('/prueba', function (Request $request, Response $response, array $args) {
     
-            $template = $request->getAttribute("twig")->load('index.html');
+            $template = $request->getAttribute("twig")->load('prueba.html');
 
             $response->getBody()->write(
-                $template->render(['name' => 'Dario'])
+                $template->render()
             );
             return $response;
         });
-
     }
-
 
 }
